193b74059b4ec8acd20ce68fc2c36220ee6dc2c6
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-06-13 21:01:47 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-import './services/helpers'
+import './js/services/helpers'
 import './scss/main.scss';
-
-import './components/header/header';
-import './components/section/section';
-import './components/footer/footer';
